c67eda673e32982ea89c93d8f65ff2ea47d27605
Igor Ponte <92915781+igorxp1@users.noreply.github.com>
2021-11-01 03:36:08 +0000

    correct bug

--- src/Componentes/Calculadora.js
+++ src/Componentes/Calculadora.js
@@ -4,53 +4,53 @@ import Visor from "./Visor.js";
 
 export default function Calculadora(props) {
   const [valor, setValor] = useState("0");
+  var result = 0;
   const [res, setRes] = useState(0);
   const [resultadoEspaco, setResultadoEspaco] = useState("");
 
   function obterValor(valor) {
     setValor(valor);
   }
- 
-  function fazerConta() {
-    setResultadoEspaco(valor.split(" "));
-    setRes(0);
 
+  function fazerConta() {
+    result = 0;
+    setResultadoEspaco(valor.split(" "));
+    console.log("1. " + result);
     for (let i = 0; i < resultadoEspaco.length; i++) {
       if (i === 1) {
         if (resultadoEspaco[i] === "+") {
-          setRes(
+          result =
             parseFloat(resultadoEspaco[i - 1]) +
-              parseFloat(resultadoEspaco[i + 1])
-          );
+            parseFloat(resultadoEspaco[i + 1]);
         } else if (resultadoEspaco[i] === "-") {
-          setRes(
+          result =
             parseFloat(resultadoEspaco[i - 1]) -
-              parseFloat(resultadoEspaco[i + 1])
-          );
+            parseFloat(resultadoEspaco[i + 1]);
         } else if (resultadoEspaco[i] === "/") {
-          setRes(
+          result =
             parseFloat(resultadoEspaco[i - 1]) /
-              parseFloat(resultadoEspaco[i + 1])
-          );
+            parseFloat(resultadoEspaco[i + 1]);
         } else if (resultadoEspaco[i] === "*") {
-          setRes(
+          result =
             parseFloat(resultadoEspaco[i - 1]) *
-              parseFloat(resultadoEspaco[i + 1])
-          );
+            parseFloat(resultadoEspaco[i + 1]);
         }
       } else {
         if (resultadoEspaco[i] === "+") {
-          setRes(res + parseFloat(resultadoEspaco[i + 1]));
+          result = result + parseFloat(resultadoEspaco[i + 1]);
         } else if (resultadoEspaco[i] === "-") {
-          setRes(res - parseFloat(resultadoEspaco[i + 1]));
+          console.log("-" + result);
+          result = result - parseFloat(resultadoEspaco[i + 1]);
+          console.log(result);
         } else if (resultadoEspaco[i] === "/") {
-          setRes(res / parseFloat(resultadoEspaco[i + 1]));
+          result = result / parseFloat(resultadoEspaco[i + 1]);
         } else if (resultadoEspaco[i] === "*") {
-          setRes(res * parseFloat(resultadoEspaco[i + 1]));
+          result = result * parseFloat(resultadoEspaco[i + 1]);
         }
       }
     }
-    // console.log("resultado- " + res);
+    //console.log(result);
+    setRes(result);
   }
 
   return (
